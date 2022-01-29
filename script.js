c61e2aed41214ab28d56370c297e6ea93ec51f8a
Danny Bimma <82052316+DannyBimma@users.noreply.github.com>
2022-01-29 17:39:07 -0400

    Challeange added and refactoring
    The first challenge from Jonas in the asynchronous JS section of his course was added, and will be tackled all weekend long. There's also been some re-arrange of commented-out code, purely for aesthetic reasons.

--- script.js
+++ script.js
@@ -3,69 +3,67 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const errorMsg = msg => {
-  countriesContainer.insertAdjacentText(`beforeend`, msg);
-  // countriesContainer.style.opacity = 1;
-};
-
-const renderCountry = function (countryData, className = ``) {
-  // display cards in on page with data from API:
-  const html = `
-   <article class="country ${className}">
-   <img class="country__img" src="${countryData.flags.svg}" />
-       <div class="country__data">
-       <h3 class="country__name">${countryData.name.official}</h3>
-       <h4 class="country__region">${countryData.region}</h4>
-       <p class="country__row"><span>👫</span>${countryData.population}</p>
-       <p class="country__row"><span>🗣</span>${
-         Object.values(countryData.languages)[0]
-       }</p>
-       <p class="country__row"><span>💰</span>${
-         Object.values(countryData.currencies)[0].name
-       }</p>
-   </div>
-   </article>`;
-
-  countriesContainer.insertAdjacentHTML(`beforeend`, html);
-  // countriesContainer.style.opacity = 1;
-};
-
 ///////////////////////////////////////
 
+/*
 // Make an AJAX call using XML-HTTP-Request:
-// const countryCard = function (country) {
-//   const request = new XMLHttpRequest();
-//   request.open(`GET`, `https://restcountries.com/v3.1/name/${country}`);
-//   request.send();
+const countryCard = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(`GET`, `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-//   // log the data from the API call:
-//   request.addEventListener(`load`, function () {
-//     console.log(this.responseText);
+  // log the data from the API call:
+  request.addEventListener(`load`, function () {
+    console.log(this.responseText);
 
-//     // parse the JSON text:
-//     const [requestData] = JSON.parse(this.responseText);
-//     console.log(requestData);
+    // parse the JSON text:
+    const [requestData] = JSON.parse(this.responseText);
+    console.log(requestData);
 
-//     // display cards in on page with data from API:
-//     const html = `
-//     <article class="country">
-//         <img class="country__img" src="${requestData.flags.svg}" />
-//         <div class="country__data">
-//         <h3 class="country__name">${requestData.name.official}</h3>
-//         <h4 class="country__region">${requestData.region}</h4>
-//         <p class="country__row"><span>👫</span>${requestData.population}</p>
-//         <p class="country__row"><span>🗣️</span>${requestData.languages.eng}</p>
-//         <p class="country__row"><span>💰</span>${requestData.currencies.BBD.name}</p>
-//     </div>
-//     </article>`;
+    // display cards in on page with data from API:
+    const html = `
+    <article class="country">
+        <img class="country__img" src="${requestData.flags.svg}" />
+        <div class="country__data">
+        <h3 class="country__name">${requestData.name.official}</h3>
+        <h4 class="country__region">${requestData.region}</h4>
+        <p class="country__row"><span>👫</span>${requestData.population}</p>
+        <p class="country__row"><span>🗣️</span>${requestData.languages.eng}</p>
+        <p class="country__row"><span>💰</span>${requestData.currencies.BBD.name}</p>
+    </div>
+    </article>`;
 
-//     countriesContainer.insertAdjacentHTML(`beforeend`, html);
-//     countriesContainer.style.opacity = 1;
-//   });
-// };
+    countriesContainer.insertAdjacentHTML(`beforeend`, html);
+    countriesContainer.style.opacity = 1;
+  });
+};
 
-// countryCard(`barbados`);
+countryCard(`barbados`);
+*/
 
+/*
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      console.log(response);
+      
+      // call json on response to read data(doing so creates another promise so return it):
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+*/
+
+/*
+// Make and AJAX call using the fetch API:
+const request = fetch(`https://restcountries.com/v3.1/name/barbados`);
+console.log(request);
+*/
+
+// Consume the promise built by the fetch API:
 /*
 const nextCountryCard = function (country) {
   // AJAX call for main country
@@ -115,69 +113,6 @@ const nextCountryCard = function (country) {
 nextCountryCard(`canada`);
 */
 
-// Make and AJAX call using the fetch API:
-// const request = fetch(`https://restcountries.com/v3.1/name/barbados`);
-// console.log(request);
-
-// Consume the promise built by the fetch API:
-/*
-const getCountryData = function (country) {
-  fetch(`https://restcountries.com/v3.1/name/${country}`)
-    .then(function (response) {
-      console.log(response);
-      // call json on response to read data(doing so creates another promise so return it):
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      renderCountry(data[0]);
-    });
-};
-*/
-
-// Create helper function (which will return a promise) to make AJAX call and convert to JSON:
-const bootJSON = function (url, errorMsg = `Whoops, something's gone awry!!`) {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`❌ ${response.status}: ${errorMsg}`);
-
-    return response.json();
-  });
-};
-
-// Replicate above with arrow functions:
-const getCountryData = function (country) {
-  bootJSON(
-    `https://restcountries.com/v3.1/name/${country}`,
-    `Country "${country}" can't be found!`
-  )
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders;
-      console.log(neighbour);
-
-      if (!neighbour)
-        throw new Error(`The country of ${country} has no bordering nation!`);
-
-      // get border country:
-      return bootJSON(
-        `https://restcountries.com/v3.1/alpha/${neighbour[0]}`,
-        `Neighbouring country for "${country}" can't be found!`
-      );
-    })
-    .then(data => renderCountry(data[0], `neighbour`))
-    .catch(error => {
-      console.error(error);
-      errorMsg(`${error.message}. Please Try Again!! `);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
-};
-
-btn.addEventListener(`click`, () => {
-  getCountryData(`Barbados`);
-});
-
 /*
 // Replicate above with arrow functions:
 const getCountryData = function (country) {
@@ -218,4 +153,122 @@ const getCountryData = function (country) {
 btn.addEventListener(`click`, () => {
   getCountryData(`spain`);
 });
+*/
+
+// Create function that renders country cards on screen:
+const renderCountry = function (countryData, className = ``) {
+  // display cards in on page with data from API:
+  const html = `
+   <article class="country ${className}">
+   <img class="country__img" src="${countryData.flags.svg}" />
+       <div class="country__data">
+       <h3 class="country__name">${countryData.name.official}</h3>
+       <h4 class="country__region">${countryData.region}</h4>
+       <p class="country__row"><span>👫</span>${countryData.population}</p>
+       <p class="country__row"><span>🗣</span>${
+         Object.values(countryData.languages)[0]
+       }</p>
+       <p class="country__row"><span>💰</span>${
+         Object.values(countryData.currencies)[0].name
+       }</p>
+   </div>
+   </article>`;
+
+  countriesContainer.insertAdjacentHTML(`beforeend`, html);
+  // countriesContainer.style.opacity = 1;
+};
+
+// Create function to display errors to users:
+const errorMsg = msg => {
+  countriesContainer.insertAdjacentText(`beforeend`, msg);
+  // countriesContainer.style.opacity = 1;
+};
+
+// Create helper function (which will return a promise) to make AJAX call and convert to JSON:
+const bootJSON = function (url, errorMsg = `Whoops, something's gone awry!!`) {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`❌ ${response.status}: ${errorMsg}`);
+
+    return response.json();
+  });
+};
+
+// Replicate above with arrow functions:
+const getCountryData = function (country) {
+  bootJSON(
+    `https://restcountries.com/v3.1/name/${country}`,
+    `The country of "${country}" can't be found!`
+  )
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders;
+      console.log(neighbour);
+
+      if (!neighbour)
+        throw new Error(`The country of ${country} has no bordering nation!`);
+
+      // get border country:
+      return bootJSON(
+        `https://restcountries.com/v3.1/alpha/${neighbour[0]}`,
+        `Neighbouring country for "${country}" can't be found!`
+      );
+    })
+    .then(data => renderCountry(data[0], `neighbour`))
+    .catch(error => {
+      console.error(error);
+      errorMsg(`${error.message}. Please Try Again!! `);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener(`click`, () => {
+  getCountryData(`Barbados`);
+});
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+In this challenge you will build a function 'whereAmI' which renders a country ONLY 
+based on GPS coordinates. For that, you will use a second API to geocode coordinates.
+
+Here are your tasks:
+
+PART 1
+
+1. Create a function 'whereAmI' which takes as inputs a latitude value (lat) and a longitude 
+value (lng) (these are GPS coordinates, examples are below).
+
+2. Do 'reverse geocoding' of the provided coordinates. Reverse geocoding means to convert coordinates 
+to a meaningful location, like a city and country name. Use this API to do reverse geocoding: 
+https://geocode.xyz/api.
+The AJAX call will be done to a URL with this format: 
+https://geocode.xyz/52.508,13.381?geoit=json. 
+Use the fetch API and promises to get the data. Do NOT use the getJSON function we created, 
+that is cheating 😉 !
+
+3. Once you have the data, take a look at it in the console to see all the attributes that you 
+received about the provided location. Then, using this data, log a message like this to the console: 
+'You are in Berlin, Germany.'
+
+4. Chain a .catch method to the end of the promise chain and log errors to the console.
+
+5. This API allows you to make only 3 requests per second. If you reload fast, you will get this 
+error with code 403. This is an error with the request. Remember, fetch() does NOT reject the promise 
+in this case. So create an error to reject the promise yourself, with a meaningful error message.
+
+PART 2
+
+6. Now it's time to use the received data to render a country. So take the relevant attribute from the 
+geocoding API result, and plug it into the countries API that we have been using.
+
+7. Render the country and catch any errors, just like we have done in the last lecture 
+(you can even copy this code, no need to type the same code)!
+
+TEST COORDINATES 1: 52.508, 13.381 (Latitude, Longitude)
+TEST COORDINATES 2: 19.037, 72.873
+TEST COORDINATES 2: -33.933, 18.474
+GOOD LUCK 😀
 */
